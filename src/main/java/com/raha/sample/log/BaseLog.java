0e72876c36197a26d2a021c2001abeb24d073178
roghayehfarhadi <r.farhadi1368@gmail.com>
2022-12-01 17:42:53 +0330

    chore: add creationTime in index

--- src/main/java/com/raha/sample/log/BaseLog.java
+++ src/main/java/com/raha/sample/log/BaseLog.java
@@ -4,9 +4,12 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @RequiredArgsConstructor
 public abstract class BaseLog {
     private final LogType logType;
+    private final LocalDateTime creationTime = LocalDateTime.now();
 }
