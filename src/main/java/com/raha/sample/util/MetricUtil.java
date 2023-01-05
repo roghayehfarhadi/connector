05c2ec51a587bae568a4fdc108dbae8f555f9f16
roghayehfarhadi <r.farhadi1368@gmail.com>
2023-01-05 19:50:29 +0330

    chore: improve MetricUtil

--- src/main/java/com/raha/sample/util/MetricUtil.java
+++ src/main/java/com/raha/sample/util/MetricUtil.java
@@ -4,63 +4,42 @@ import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Component
 public class MetricUtil {
-
+    private final Map<String, Counter> counterMap = new ConcurrentHashMap<>();
+    private final MeterRegistry meterRegistry;
     private static final String TOPIC = "topic";
     private static final String TYPE = "type";
     private static final String PUBLISHED_TYPE = "published";
     private static final String CONSUMED_TYPE = "consumed";
-    private static final String MOON_METRIC_NAME = "moon_message_counter";
-    private static final String SUN_METRIC_NAME = "sun_message_counter";
-    private final String moonTopic;
-    private final String sunTopic;
-    private final Counter moonPublishedMessage;
-    private final Counter moonConsumedMessage;
-    private final Counter sunPublishedMessage;
-    private final Counter sunConsumedMessage;
+    private static final String METRIC_NAME = "kafka_messages";
 
-
-    public MetricUtil(MeterRegistry meterRegistry, String moonTopic, String sunTopic) {
-        this.moonTopic = moonTopic;
-        this.sunTopic = sunTopic;
-
-        this.moonPublishedMessage = Counter.builder(MOON_METRIC_NAME)
-                .tag(TYPE, PUBLISHED_TYPE)
-                .tag(TOPIC, moonTopic)
-                .register(meterRegistry);
-
-        this.moonConsumedMessage = Counter.builder(MOON_METRIC_NAME)
-                .tag(TYPE, CONSUMED_TYPE)
-                .tag(TOPIC, moonTopic)
-                .register(meterRegistry);
-
-
-        this.sunPublishedMessage = Counter.builder(SUN_METRIC_NAME)
-                .tag(TYPE, PUBLISHED_TYPE)
-                .tag(TOPIC, sunTopic)
-                .register(meterRegistry);
-
-        this.sunConsumedMessage = Counter.builder(SUN_METRIC_NAME)
-                .tag(TYPE, CONSUMED_TYPE)
-                .tag(TOPIC, sunTopic)
-                .register(meterRegistry);
+    public MetricUtil(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
     }
 
     public void incrementPublishedMessage(String topic) {
-        if (topic.equals(sunTopic)) {
-            this.sunPublishedMessage.increment();
-        } else if (topic.equals(moonTopic)) {
-            this.moonPublishedMessage.increment();
-        }
+        countEvent(topic, PUBLISHED_TYPE);
     }
 
 
     public void incrementConsumedMessage(String topic) {
-        if (topic.equals(sunTopic)) {
-            this.sunConsumedMessage.increment();
-        } else if (topic.equals(moonTopic)) {
-            this.moonConsumedMessage.increment();
+        countEvent(topic, CONSUMED_TYPE);
+    }
+
+    public void countEvent(String topic, String type) {
+        String key = topic + "-" + type;
+        var counter = counterMap.get(key);
+        if (counter == null) {
+            counter = Counter.builder(METRIC_NAME)
+                    .tag(TYPE, type)
+                    .tag(TOPIC, topic)
+                    .register(meterRegistry);
+            counterMap.put(key, counter);
         }
+        counter.increment();
     }
 }
